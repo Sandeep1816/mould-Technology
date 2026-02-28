5548728dcac244c90012a8a73e941ec34930c9de
Sandeep1816 <avalasandeep02@gmail.com>
2026-02-28 14:38:01 +0530

    config image quality bug removed

--- next.config.mjs
+++ next.config.mjs
@@ -3,29 +3,31 @@
 
 // /** @type {import('next').NextConfig} */
 // const nextConfig = {
-//   typescript: {
-//     ignoreBuildErrors: true,
-//   },
-
 //   images: {
-//     unoptimized: true, // OK for dev & external images
-//     domains: [
-//       "images.unsplash.com",   // ✅ REQUIRED
-//       "source.unsplash.com",
-//       "upload.wikimedia.org",
-//       "placehold.co",
-//       "randomuser.me",
-//        "res.cloudinary.com",
+//     remotePatterns: [
+//       { protocol: "https", hostname: "images.unsplash.com" },
+//       { protocol: "https", hostname: "source.unsplash.com" },
+//       { protocol: "https", hostname: "upload.wikimedia.org" },
+//       { protocol: "https", hostname: "placehold.co" },
+//       { protocol: "https", hostname: "randomuser.me" },
+//       { protocol: "https", hostname: "res.cloudinary.com" },
+//       { protocol: "https", hostname: "i.pravatar.cc" },
+
+//       // ✅ ADD THIS
+//       { protocol: "https", hostname: "ui-avatars.com" },
+
+//       { protocol: "http", hostname: "localhost", port: "5000" },
 //     ],
 //   },
 // }
 
 // export default nextConfig
 
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    qualities: [70, 75], // ✅ allow quality 70
+
     remotePatterns: [
       { protocol: "https", hostname: "images.unsplash.com" },
       { protocol: "https", hostname: "source.unsplash.com" },
@@ -34,10 +36,7 @@ const nextConfig = {
       { protocol: "https", hostname: "randomuser.me" },
       { protocol: "https", hostname: "res.cloudinary.com" },
       { protocol: "https", hostname: "i.pravatar.cc" },
-
-      // ✅ ADD THIS
       { protocol: "https", hostname: "ui-avatars.com" },
-
       { protocol: "http", hostname: "localhost", port: "5000" },
     ],
   },
